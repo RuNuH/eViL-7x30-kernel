49241a9c6c13dafe8b2e80307ee2b9450dfa32fa
Daniel Hall <runuh@comcast.net>
2010-11-28 19:35:50 -0800

    Initial push

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 68 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#68 PREEMPT Fri Nov 26 22:48:56 PST 2010"
+#define LINUX_COMPILE_TIME "22:48:56"
+#define LINUX_COMPILE_BY "dee"
+#define LINUX_COMPILE_HOST "dee-desktop"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
